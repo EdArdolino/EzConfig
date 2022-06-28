a9024ac531a2b95732fa7faeae7f3ee376e33a1b
Ed Ardolino <edwardardolino@gmail.com>
2022-06-28 18:41:18 -0400

    Added run_again() function, started work on writing to config files

--- EZConfigFunctions.py
+++ EZConfigFunctions.py
@@ -13,16 +13,16 @@
 # Function to start the program and take user input
 def start():
     print("\n Choose which config file you would like to update:\n 1.) Alias \n 2.) Crontab")
-    response  = input("Please enter a number from the list above:")
+    response = input("Please enter a number from the list above:")
     while response not in {"1", "2"}:
-            response = input("Invalid input, please enter a number from the list above")
+        response = input("Invalid input, please enter a number from the list above")
 
     # If statement to take binary input and select the alias function
     if response == "1":
         print("\n Which shell are you using?:\n 1.) Bash \n 2.) Zsh \n 3.) Fish \n")
         shellresponse = input("Please enter a number from the list above:")
-        while shellresponse not in {"1" , "2", "3"}:
-                shellresponse = input("Invalid input, please enter a number from the list above.")
+        while shellresponse not in {"1", "2", "3"}:
+            shellresponse = input("Invalid input, please enter a number from the list above.")
 
     if shellresponse == "1":
         aliasbash()
@@ -38,37 +38,66 @@ def start():
         crontab()
 
     # Asks the user if they would like to run the program again
-    #run_again()
+    run_again()
+
 
 # Writes to bash
+# Working on writing to ~/.bashrc, not currently writing to file
 def write_to_bash(string):
-    with open ("~/.bashrc", "a") as file:
+    with open("~/.bashrc", "a") as file:
         file.writelines(str(string))
+        file.close()
+
 
 # Writes to zsh
+# Working on writing to ~/.zshrc, not currently writing to file
 def write_to_zsh(string):
-    with open ("~/.zshrc", "a") as file:
+    with open("~/.zshrc", "a") as file:
         file.writelines(str(string))
+        file.close()
+
 
 # Writes to fish
+# Working on writing to ~/.config/fish/config.fish, not currently writing to file
 def write_to_fish(string):
-    with open ("~/.config/fish/config/fish") as file:
+    with open("~/.config/fish/config.fish") as file:
         file.writelines(str(string))
+        file.close()
+
 
 # Function to update the crontab file
 def crontab():
     print("In progress")
 
+
 # Function to input an alias and write it to the alias file (bash)
 def aliasbash():
     print("Please enter the alias you would like to add to ~/.bashrc:\n")
-    #bashresponse = input ()
+    bashresponse = input("Alias: ")
+    # In progress to write to file
+
 
 # Function to input an alias and write it to the alias file (bash)
 def aliaszsh():
     print("Please enter the alias you would like to add to ~/.zshrc:\n")
+    zshresponse = input("Alias: ")
+    # In progress to write to file
 
 # Function to input an alias and write it to the alias file (bash)
 def aliashfish():
     print("Please enter the alias you would like to add to ~/.config/fish/config.fish:\n")
+    fishresponse = input("Alias: ")
+    # In progress to write to file
 
+# Function to run the program again
+def run_again():
+    add_more = input("\n Would you like to run EZConfig.py again? (y/n): ").lower()
+    while add_more not in {"y", "n"}:
+        add_more = input("Incorrect input, please enter y or n: ")
+
+    # If statement to either run the program again or terminate
+    if add_more == "y":
+        start()
+    elif "n":
+        print("\n Thank you. \n")
+        exit(0)
